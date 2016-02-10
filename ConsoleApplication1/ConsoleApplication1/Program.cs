a04896c9e80363641011fc1eac10b971eb905ec8
Jean-Paul Goodwin <goodwinjeanpaul@outlook.fr>
2016-02-10 22:44:50 +0100

    Initial Commit avec code de Vernam

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytpologie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Vernam
+    {
+        public static int[] chiffrer(int[] parMessage, int[] parCle)
+        {
+            int[] retour = new int[parMessage.Length];
+            for (int i = 0; i < parMessage.Length; i++)
+            {
+                retour[i] = (parMessage[i] + parCle[i % parCle.Length]) % 2;
+            }
+            return retour;
+        }
+    }
+}
