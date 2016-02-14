0759af8a8d44819888ef488907312edd2f298061
Jean-Paul Goodwin <goodwinjeanpaul@outlook.fr>
2016-02-14 18:36:59 +0100

    Ajout de l'algorithme d'euclide étendu Ajout du calcul de l'inverse d'un nombre n modulo m

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace Crytpologie
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--Chiffrage du message--");
             int[] message = {1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0 }; //message a chiffrer
             int[] cle = { 1, 1, 0 };    //cle de chiffrage
             int[] res = Vernam.chiffrer(message, cle);  //chiffrage du message
@@ -17,6 +18,9 @@ namespace Crytpologie
             {
                 Console.Write(res[i]);
             }
+            Console.WriteLine("\n--Test Algo Etendu--");
+            int[] tab = Euclide.algoEtendu(26, 15);
+            Console.WriteLine(tab[0] + " " + tab[1]+"\n--Inverse--\n"+ Euclide.inverse(51, 242));
             Console.WriteLine("");
         }
     }
@@ -31,6 +35,34 @@ namespace Crytpologie
                 retour[i] = (parMessage[i] + parCle[i % parCle.Length]) % 2;
             }
             return retour;
+        }
+    }
+
+    class Euclide
+    {
+        public static int[] algoEtendu(int a, int b)
+        {
+            if (a<b || b<1)
+                throw new Exception("a<b ou b<1");
+            int r = a % b;
+            int q = (a - r) / b;
+            if (r == 1)
+            {
+                int[] tab = { r, -q };
+                return tab;
+            }
+            else
+            {
+                int[] tab = algoEtendu(b, r);
+        tab[0] = tab[0]-(q* tab[1]);
+                int t = tab[0]; tab[0] = tab[1]; tab[1] = t;
+                return tab;
+            }
+        }
+    
+        public static int inverse(int n, int m)
+        {  
+            return Euclide.algoEtendu(m, n)[1];
         }
     }
 }
