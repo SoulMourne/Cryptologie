3be1a25760aaf58e001a5961315a44bd3161c50e
Jean-Paul Goodwin <goodwinjeanpaul@outlook.fr>
2016-02-14 19:13:00 +0100

    Ajout de la décomposition primaire d'un nombre n et du calcul de Phi d'un nombre n

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -21,6 +21,13 @@ namespace Crytpologie
             Console.WriteLine("\n--Test Algo Etendu--");
             int[] tab = Euclide.algoEtendu(26, 15);
             Console.WriteLine(tab[0] + " " + tab[1]+"\n--Inverse--\n"+ Euclide.inverse(51, 242));
+            Console.WriteLine("\n--Décomposition primaire de 54--\n");
+            Dictionary<int, int> resultat = Primaire.decompose_primaire(54);
+            Dictionary<int, int>.KeyCollection cles= resultat.Keys;
+            foreach(int valeur in cles)
+            {
+                Console.WriteLine(valeur + "^" + resultat[valeur] + "*");
+            }
             Console.WriteLine("");
         }
     }
@@ -63,6 +70,74 @@ namespace Crytpologie
         public static int inverse(int n, int m)
         {  
             return Euclide.algoEtendu(m, n)[1];
+        }
+    }
+
+    public class Primaire
+    {
+        public static bool[] crible(int n)
+        {
+            int max = (int)Math.Sqrt(n);
+            bool[] crible = new bool[n];
+            for (int i = 0; i < n; i++)
+                crible[i] = true;
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (crible[i])
+                    for (int j = 2; j < n; j++)
+                    {
+                        Console.WriteLine(i + "*" + j);
+                        if (i * j < crible.Length)
+                            crible[i * j] = false;
+                    }
+                // System.out.println("------\nA : "+n+"\nB : "+i+"\nQ : "+q);
+            }
+            crible[0] = false;
+            crible[1] = false;
+            return crible;
+        }
+
+        public static Dictionary<int, int> decompose_primaire(int n)
+        {
+            Dictionary<int, int> retour = new Dictionary<int,int>();
+            int i = 2;
+            while (i <= n)
+            {
+                if (n % i == 0)
+                {
+                    if (retour.ContainsKey(i))
+                    {
+                        try
+                        {
+                            retour[i]++;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    else
+                        retour.Add(i, 1);
+                    n /= i;
+                    i = 1;
+                }
+                i++;
+            }
+            return retour;
+        }
+
+        public static int euler(int n)
+        {
+            Dictionary<int,int> decompPrime = Primaire.decompose_primaire(n);
+            int resultat = 1;
+            Dictionary<int,int>.KeyCollection cles = decompPrime.Keys;
+            foreach (int valeur in cles)
+            {
+                int power = decompPrime[valeur];
+                resultat *= (int)(Math.Pow((int)valeur, (int)power) - Math.Pow((int)valeur, (int)power - 1));
+            }
+            return resultat;
         }
     }
 }
